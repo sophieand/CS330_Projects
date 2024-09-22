6507b5e7db4a2ab9e926eb7ea9201e6b12479fd2
scanderson6 <scanderson6@crimson.ua.edu>
2024-09-22 16:51:10 -0500

    Current version, still needs a few fixes

---
+++ 2024Fall-Assignment1-scanderson6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace _2024Fall_Assignment1_scanderson6.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
